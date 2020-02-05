9e2047a31e4b47a6802c7e7402a8812b9ac50c1a
MLOPEZMALET <49145665+MLOPEZMALET@users.noreply.github.com>
2020-02-05 22:10:35 +0100

    actu

--- commandes.sh
+++ commandes.sh
@@ -100,4 +100,3 @@ sudo service nginx restart
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt install python-certbot-nginx
 sudo certbot --nginx -d ceptyconsultant.local -d www.ceptyconsultant.local
-
