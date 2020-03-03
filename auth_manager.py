cee4b8e5b7cbadc148a50b76e43ffe0316d60587
MLOPEZMALET <melanie.lopezmalet@gmail.com>
2020-03-03 12:46:31 +0100

    clean

--- auth_manager.py
+++ auth_manager.py
@@ -8,5 +8,13 @@ def read_data(path):
         data = json.load(f)
         return data
 
+def add_user(new_data, path):
+    #if new_data
+    data = read_data(path)
+    data.append(new_data)
+    with open(path, 'w') as outfile:
+        json.dump(data, outfile)
 
-print(read_data(path_users))
+
+
+print(read_data(path_users)["user_name"])
